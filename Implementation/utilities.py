e44430b1adf32e4fcd51ca1574c36e02bd8a4d6a
Hasan Shahriar <hshahriar75@gmail.com>
2024-04-03 09:48:06 -0400

    Update utilities.py

--- Implementation/utilities.py
+++ Implementation/utilities.py
@@ -5,11 +5,11 @@ class Utilities:
     Define utility functions for calculating distance between two cities and total distance of a route
     '''
 
-    def __init__(self, cities: list, city1: int, city2: int, route: list) -> None:
+    def __init__(self, cities: list) -> None:
         self.cities = cities
-        self.city1 = city1
-        self.city2 = city2
-        self.route = route
+        # self.city1 = city1
+        # self.city2 = city2
+
 
     def distance(self):
         '''
@@ -18,7 +18,7 @@ class Utilities:
         return np.sqrt((self.city1[0] - self.city2[0]) ** 2 + (self.city1[1] - self.city2[1]) ** 2)
 
     # Calculate total distance of a route
-    def total_distance(self) -> int:
+    def total_distance(self, route) -> int:
         '''
         iterate over each cities in the given route and sums the distance.
         Finally adds the distance from end city to starting city distance to complete the cycle.
@@ -26,11 +26,11 @@ class Utilities:
 
         dist = 0
 
-        for i in range(len(self.route) - 1):
-            from_city = self.route[i]
-            to_city = self.route[i+1]
+        for i in range(len(route) - 1):
+            from_city = route[i]
+            to_city = route[i+1]
             dist += self.cities[from_city][to_city]
 
         # Return to the starting city
-        dist += self.cities[self.route[-1]][self.route[0]]
+        dist += self.cities[route[-1]][route[0]]
         return dist
